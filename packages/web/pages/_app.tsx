1e0b224ce746ff14edb8419c1ac49485ca37fbce
Jesper Paulsen <jesper@jesper.no>
2021-10-13 16:07:41 +0200

    add tailwind

---
+++ packages/web/pages/_app.tsx
@@ -0,0 +1,9 @@
+import 'tailwindcss/tailwind.css'
+import { NextPage,  } from 'next'
+import type { AppProps } from 'next/app'
+
+const DataCollectorPage: NextPage<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default DataCollectorPage
---
+++ packages/web/pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+import { NextPage } from 'next'
+
+
+const Index: NextPage = () => {
+  return <div className="text-4xl">Hello</div>
+}
+
+export default Index
