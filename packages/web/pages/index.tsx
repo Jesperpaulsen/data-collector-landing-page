6cea67c9e47d72dd7dd255515625e37b7053781c
Jesper Paulsen <jesper@jesper.no>
2021-10-14 15:54:28 +0200

    fix mobile layout

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -3,12 +3,12 @@ import { NextPage } from 'next'
 
 
 const Index: NextPage = () => {
-  return <div className="flex flex-col justify-center h-screen items-center">
+  return <div className="flex flex-col justify-center h-screen items-center text-center p-4 text-gray-800">
     <div>
       Have you ever wondered how your internet habits are effecting the climate?
     </div>
-    <div>
-      Sign up to get a better understanding of your digital climate footprint.
+    <div className="text-black">
+      Soon you will be able to sign up to get a better understanding of your digital climate footprint.
     </div>
   </div>
 }
