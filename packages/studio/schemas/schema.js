397ab86b0f438164c7fd642395ce542f61a6b122
Jesper Paulsen <jesper@jesper.no>
2021-10-15 08:55:20 +0200

    fix schemas

--- packages/studio/schemas/schema.js
+++ packages/studio/schemas/schema.js
@@ -1,32 +1,9 @@
-import createSchema from 'part:@sanity/base/schema-creator'
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import about from './pages/about'
-import home from './pages/home'
-import investments from './pages/investments'
-import news from './pages/news'
-
-import defaultImage from './defaultImage'
-import investment from './investment'
-import investmentCategory from './investmentCategory'
-import location from './location'
-import newsArticle from './newsArticle'
-import blockContent from './blockContent'
-
-const schemas = [
-  about,
-  home,
-  investments,
-  news,
-  defaultImage,
-  investment,
-  investmentCategory,
-  location,
-  newsArticle,
-  blockContent
-]
+const schemas = [];
 
 export default createSchema({
-  name: 'default',
-  types: schemaTypes.concat(schemas)
-})
+  name: "default",
+  types: schemaTypes.concat(schemas),
+});
