24e3ea62d4583dca472ad816644a93b56b9df153
Jesper Paulsen <jesper@jesper.no>
2021-10-20 11:52:48 +0200

    update head

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,15 +1,21 @@
 import "../styles/index.css";
 import { NextPage } from "next";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import Menu from "../components/Menu";
 
 const DataCollectorPage: NextPage<AppProps> = ({ Component, pageProps }) => {
   pageProps.className = "flex-grow";
   return (
-    <div className="h-screen flex flex-col">
-      <Menu />
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        Data Collector - raising awarenes around the digital climate footprint
+      </Head>
+      <div className="h-screen flex flex-col">
+        <Menu />
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 };
 
