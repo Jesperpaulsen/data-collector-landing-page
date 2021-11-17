0bfb057a0d835f122e26fafc300dc07de41cbcaa
Jesper Paulsen <jesper@jesper.no>
2021-11-17 10:04:24 +0100

    accept all emails

--- packages/web/components/SignUpForm.tsx
+++ packages/web/components/SignUpForm.tsx
@@ -9,7 +9,7 @@ const SignUpForm: React.FC<ISignUpForm> = ({ title, description }) => {
 
   const checkEmail = (email: string) => {
     const re =
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@gmail.com/g;
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   };
 
